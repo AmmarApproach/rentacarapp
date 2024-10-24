e17ba76c67f78f3b47f18fa3b639ef2199cfb0d6
Ammar Hasan <ammar.hasan@approach.be>
2024-10-24 15:20:12 +0200

    Secure By Design Principle

--- src/main/java/org/approach/AdminResource.java
+++ src/main/java/org/approach/AdminResource.java
@@ -1,16 +1,27 @@
 package org.approach;
 
+import jakarta.annotation.security.PermitAll;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/api/admin")
+@RolesAllowed("ADMIN")
 public class AdminResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String adminResource() {
         return "admin";
+    }
+
+    @GET
+    @Path("/contact")
+    @Produces(MediaType.TEXT_PLAIN)
+    @PermitAll
+    public String adminContact() {
+        return "contact of the admin: ...";
     }
 }
--- src/main/java/org/approach/UserResource.java
+++ src/main/java/org/approach/UserResource.java
@@ -1,11 +1,13 @@
 package org.approach;
 
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.SecurityContext;
 
 @Path("/api/users")
+@RolesAllowed("USER")
 public class UserResource {
 
     @GET
